0abe8ff7a64d7bb7b181e247c82770683ca8a64a
Ankur Mishra <2018ankurm@gmail.com>
2024-06-30 16:56:44 -0400

    setup a learning rate sweep

--- hpo.py
+++ hpo.py
@@ -1,7 +1,6 @@
 from clearml.automation import LogUniformParameterRange, UniformIntegerParameterRange
 from clearml.automation import HyperParameterOptimizer
-# from clearml.automation.optuna import OptimizerOptuna
-from BinarySearch import BinarySearch
+from clearml.automation.optuna import OptimizerOptuna
 from clearml import Task
 import numpy as np
 import hydra
@@ -11,125 +10,96 @@ import os
 from train import Config
 import subprocess
 
-args = {
-    "template_task_id": '69f514621af04bb69ec5e8e84b948635',
-    "run_as_service": False,
-}
+
+def get_base_template_id(config: Config):
+    git_branch_name = subprocess.run(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    ).stdout.strip()
+    config_name = hydra.core.hydra_config.HydraConfig.get()["job"]["config_name"]
+    project_name = f"{config_name}/{git_branch_name}"
+    task_name = config.paths.model_name
+
+    print(f"{project_name=}")
+    print(f"{task_name=}")
+    return Task.get_task(
+        project_name=f"{config_name}/{git_branch_name}",
+        task_name=task_name,
+    ).id
 
 
-def create_hpo_task(config: Config):
-    if not args["template_task_id"]:
-        git_branch_name = subprocess.run(
-            ["git", "rev-parse", "--abbrev-ref", "HEAD"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        ).stdout.strip()
-        config_name = hydra.core.hydra_config.HydraConfig.get()["job"]["config_name"]
-        project_name = f"{config_name}/{git_branch_name}"
-        task_name = config.paths.model_name
 
-        print(f"{project_name=}")
-        print(f"{task_name=}")
-        args["template_task_id"] = Task.get_task(
-            project_name=f"{config_name}/{git_branch_name}",
-            task_name=config.paths.model_name,
-        ).id
-
-    return HyperParameterOptimizer(
-        # specifying the task to be optimized, task must be in system already so it can be cloned
-        #   base_task_id=TEMPLATE_TASK_ID,
-        base_task_id=args["template_task_id"],  # 2fbbd3a9216d4e9c963b48b70fcebd89
-        # setting the hyperparameters to optimize
-        hyper_parameters=[
-            LogUniformParameterRange(
-                "Hydra/training.learning_rate", min_value=-5, max_value=0
-            )
-        ],
-        # setting the objective metric we want to maximize/minimize
-        objective_metric_title="loss",
-        objective_metric_series="loss",
-        objective_metric_sign="min",
-        # setting optimizer
-        optimizer_class=BinarySearch,
-        # configuring optimization parameters
-        execution_queue=config.training.queue,
-        max_number_of_concurrent_tasks=1,
-        #   optimization_time_limit=120.,
-        #   compute_time_limit=1200,
-        total_max_jobs=2,
-        min_iteration_per_job=1,
-        max_iteration_per_job=150000,
-    )
-
-
-def job_complete_callback(
-    job_id,  # type: str
-    objective_value,  # type: float
-    objective_iteration,  # type: int
-    job_parameters,  # type: dict
-    top_performance_job_id,  # type: str
+def lr_sweep_binary_search(
+    lower_bound, upper_bound, template_task_id, max_iterations=5, queue_name="default"
 ):
-    print(
-        "Job completed!", job_id, objective_value, objective_iteration, job_parameters
-    )
-    if job_id == top_performance_job_id:
-        print(
-            "WOOT WOOT we broke the record! Objective reached {}".format(
-                objective_value
-            )
+    parent_task = Task.init(project_name="LR Sweep", task_name="Binary Search LR Sweep")
+    pt_logger = parent_task.get_logger()
+    best_lr = None
+    best_loss = float("inf")
+
+    loss_per_learning_rate = {} 
+    
+    def train (learning_rate, template_task_id):
+        # Clone the template task and override the learning rate
+        child_task = Task.clone(
+            source_task=template_task_id, name=f"LR Sweep - LR {learning_rate:.6f}"
         )
+        child_task.set_parameter("Hydra/training.learning_rate", learning_rate ) 
+        print(f"training model with lr: {learning_rate}")
+        # Enqueue the child task for execution
+        Task.enqueue(child_task.id, queue_name=queue_name)
+
+        # Wait for the child task to complete
+        child_task.wait_for_status()
+
+        # Get the results from the child task
+        child_task_results = child_task.get_reported_scalars()
+        # print(f"{child_task_results=}")
+        # print(f"{child_task_results['loss']=}")
+        return child_task_results["loss"]["loss"]["y"][-1]
+   
+    for i in range(max_iterations): 
+        lr_samples = 10 ** np.random.uniform(np.log10(lower_bound), np.log10(upper_bound), 5)
+
+        for lr in lr_samples:
+            loss = train(lr, template_task_id)
+            loss_per_learning_rate[lr] = loss
+        
+            # Log the results in the parent task
+            pt_logger.report_scalar(
+                "learning_rate", "value", lr, iteration=i
+            )
+            if loss < best_loss:
+                best_lr = lr 
+                best_loss = loss
+            
+        pt_logger.report_scalar("loss", "value", loss, iteration=i)
+
+        print(f"Iteration {i+1}: LR = {best_lr:.6f}, Loss = {best_loss:.6f}")
+        print(f"Bounds = [{lower_bound:.6f}, {upper_bound:.6f}]")
+        # Update best learning rate if necessary
+        lower_bound = np.power(10, np.log10(best_lr) - .5)
+        upper_bound = np.power(10, np.log10(best_lr) + .5)
+
+        # Check for convergence
+        if np.isclose(lower_bound, upper_bound, rtol=1e-5):
+            break
+
+    print(f"\nBest learning rate found: {best_lr:.6f} with loss: {best_loss:.6f}")
+    pt_logger.report_scalar("best_learning_rate", "value", best_lr)
+    pt_logger.get_logger().report_scalar("best_loss", "value", best_loss)
+
+    parent_task.close()
 
 
 @hydra.main(config_path="configs", version_base=None)
 def main(config):
     config = jax_extra.make_dataclass_from_dict(Config, config)
-    if config.training.queue:
-        # config_name = hydra.core.hydra_config.HydraConfig.get()['job']['config_name']
-        git_branch_name = subprocess.run(
-            ["git", "rev-parse", "--abbrev-ref", "HEAD"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        ).stdout.strip()
-
-        task = Task.init(
-            project_name="HPO",
-            task_name=f"Learning rate search {config.paths.model_name}",
-            task_type=Task.TaskTypes.optimizer,
-            reuse_last_task_id=False,
-        )
-        # task.execute_remotely(queue_name='seqax', exit_process=True)
-
-        optimizer = create_hpo_task(config)
-        # report every 12 seconds, this is way too often, but we are testing here J
-        optimizer.set_report_period(30)
-        # start the optimization process, callback function to be called every time an experiment is completed
-        # this function returns immediately
-        optimizer.start(job_complete_callback=job_complete_callback)
-
-        # set the time limit for the optimization process (2 hours)
-        optimizer.set_time_limit(in_minutes=120.0)
-        # wait until process is done (notice we are controlling the optimization process in the background)
-        optimizer.wait()
-        # optimization is completed, print the top performing experiments id
-        top_exp = optimizer.get_top_experiments(top_k=5)
-        print([t.id for t in top_exp])
-        # make sure background optimization stopped
-        optimizer.stop()
-
-        print("We are done, good bye")
-
-        # Step 4: Finalize
-        task.close()
-
-
+    template_task_id = get_base_template_id(config) 
+    lr_sweep_binary_search(1e-5, 1.0, template_task_id, queue_name=config.training.queue)
+    
 if __name__ == "__main__":
     main()
-
-
-# Step 2: Define the initial search space in log scale
-
-# Function to create a new HPO task with a specified learning rate range

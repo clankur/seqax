2c7fc62866b092ea86369173b1cfb6c5162a9141
Ankur Mishra <2018ankurm@gmail.com>
2024-07-01 13:37:31 -0400

    actual binary search

--- hpo.py
+++ hpo.py
@@ -41,7 +41,6 @@ def lr_sweep_binary_search(
     best_loss = float("inf")
 
     loss_per_learning_rate = {} 
-    
     def train (learning_rate, template_task_id):
         # Clone the template task and override the learning rate
         child_task = Task.clone(
@@ -49,57 +48,64 @@ def lr_sweep_binary_search(
         )
         child_task.set_parameter("Hydra/training.learning_rate", learning_rate ) 
         print(f"training model with lr: {learning_rate}")
-        # Enqueue the child task for execution
-        Task.enqueue(child_task.id, queue_name=queue_name)
-
-        # Wait for the child task to complete
-        child_task.wait_for_status()
+        for i in range(2):
+            try:
+                Task.enqueue(child_task.id, queue_name=queue_name)
+                child_task.wait_for_status()
+                break
+            except RuntimeError as e:
+                child_task = Task.clone(
+                    source_task=child_task.id, name=f"LR Sweep {i+1} - LR {learning_rate:.6f}"
+                )
+                if i == 1:
+                    raise e
+                print(e)
 
         # Get the results from the child task
         child_task_results = child_task.get_reported_scalars()
         # print(f"{child_task_results=}")
         # print(f"{child_task_results['loss']=}")
         return child_task_results["loss"]["loss"]["y"][-1]
-   
+  
+    def get_loss (lr):
+        lr = 10 ** lr
+        if lr not in loss_per_learning_rate:
+            loss_per_learning_rate[lr] = train(lr, template_task_id)
+        return loss_per_learning_rate[lr]
+    
     for i in range(max_iterations): 
-        lr_samples = 10 ** np.random.uniform(np.log10(lower_bound), np.log10(upper_bound), 5)
-
-        for lr in lr_samples:
-            loss = train(lr, template_task_id)
-            loss_per_learning_rate[lr] = loss
+        midpoint = (lower_bound + upper_bound) / 2
+        low_loss, up_loss = get_loss(lower_bound), get_loss(upper_bound)
         
-            # Log the results in the parent task
-            pt_logger.report_scalar(
-                "learning_rate", "value", lr, iteration=i
-            )
-            if loss < best_loss:
-                best_lr = lr 
-                best_loss = loss
-            
+        if low_loss < up_loss:
+            upper_bound = midpoint
+            loss, lr = low_loss, lower_bound
+        else:
+            lower_bound = midpoint
+            loss, lr = up_loss, upper_bound
+
+        if loss < best_loss or i == max_iterations -1 :
+            best_loss, best_lr = loss, 10 ** lr
+           
         pt_logger.report_scalar("loss", "value", loss, iteration=i)
 
-        print(f"Iteration {i+1}: LR = {best_lr:.6f}, Loss = {best_loss:.6f}")
+        print(f"Iteration {i+1}: LR = {lr:.6f}, Loss = {loss:.6f}")
         print(f"Bounds = [{lower_bound:.6f}, {upper_bound:.6f}]")
-        # Update best learning rate if necessary
-        lower_bound = np.power(10, np.log10(best_lr) - .5)
-        upper_bound = np.power(10, np.log10(best_lr) + .5)
 
         # Check for convergence
         if np.isclose(lower_bound, upper_bound, rtol=1e-5):
             break
 
     print(f"\nBest learning rate found: {best_lr:.6f} with loss: {best_loss:.6f}")
-    pt_logger.report_scalar("best_learning_rate", "value", best_lr)
-    pt_logger.get_logger().report_scalar("best_loss", "value", best_loss)
 
     parent_task.close()
-
 
 @hydra.main(config_path="configs", version_base=None)
 def main(config):
     config = jax_extra.make_dataclass_from_dict(Config, config)
-    template_task_id = get_base_template_id(config) 
-    lr_sweep_binary_search(1e-5, 1.0, template_task_id, queue_name=config.training.queue)
+    template_task_id = get_base_template_id(config)
+    lower, upper = np.log10(5e-4), np.log10(5e-2)
+    lr_sweep_binary_search(lower, upper, template_task_id, queue_name=config.training.queue)
     
 if __name__ == "__main__":
     main()

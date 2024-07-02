0445fa1557160965e2209c1769c0fd5170d1d3a9
Ankur Mishra <2018ankurm@gmail.com>
2024-07-02 16:51:51 -0400

    cleanup a bit more

--- hpo.py
+++ hpo.py
@@ -4,9 +4,16 @@ import hydra
 import jax_extra
 from train import Config
 import subprocess
+from dataclasses import dataclass
+from typing import Optional
 
+@dataclass
+class Config:
+    model_name: str
+    queue_name: str
+    project_name: Optional[str] = None
 
-def get_base_template_id(config: Config):
+def get_task_details(config: Config):
     git_branch_name = subprocess.run(
         ["git", "rev-parse", "--abbrev-ref", "HEAD"],
         check=True,
@@ -15,21 +22,18 @@ def get_base_template_id(config: Config):
         text=True,
     ).stdout.strip()
     config_name = hydra.core.hydra_config.HydraConfig.get()["job"]["config_name"]
-    project_name = f"{config_name}/{git_branch_name}"
-    task_name = config.paths.model_name
+    project_name = config.project_name if config.project_name else f"{config_name}/{git_branch_name}"
+    task_name = config.model_name
 
-    print(f"{project_name=}")
-    print(f"{task_name=}")
-    return Task.get_task(
-        project_name=f"{config_name}/{git_branch_name}",
-        task_name=task_name,
-    ).id
-
-
+    return project_name, task_name
+    
 def lr_sweep_binary_search(
-    lower_bound, upper_bound, template_task_id, max_iterations=5, queue_name="default"
+    config: Config, lower_bound, upper_bound, template_task_id, max_iterations=5 
 ):
-    parent_task = Task.init(project_name="LR Sweep", task_name="Binary Search LR Sweep")
+    config_name = hydra.core.hydra_config.HydraConfig.get()["job"]["config_name"]
+    project_name = f"{config_name}/lr_sweep"
+    task_name = f"{config.model_name}_lr_sweep"
+    parent_task = Task.init(project_name=project_name, task_name=task_name)
     pt_logger = parent_task.get_logger()
     best_lr = None
     best_loss = float("inf")
@@ -38,22 +42,22 @@ def lr_sweep_binary_search(
     def train (learning_rate, template_task_id):
         # Clone the template task and override the learning rate
         child_task = Task.clone(
-            source_task=template_task_id, name=f"LR Sweep - LR {learning_rate:.6f}"
+            source_task=template_task_id, name=f"{task_name}_lr:{learning_rate:.6f}"
         )
         child_task.set_parameter("Hydra/training.learning_rate", learning_rate ) 
         print(f"training model with lr: {learning_rate}")
         for i in range(3):
             try:
-                Task.enqueue(child_task.id, queue_name=queue_name)
+                Task.enqueue(child_task.id, queue_name=config.queue_name)
                 child_task.wait_for_status()
                 break
             except RuntimeError as e:
-                child_task = Task.clone(
-                    source_task=child_task.id, name=f"LR Sweep {i+1} - LR {learning_rate:.6f}"
-                )
-                if i +1 == 3:
+                if i + 1 == 3:
                     raise e
                 print(e)
+                child_task = Task.clone(
+                    source_task=child_task.id, name=f"{task_name}_lr:{learning_rate:.6f}"
+                )
 
         # Get the loss from the child task
         child_task_results = child_task.get_reported_scalars()
@@ -95,9 +99,16 @@ def lr_sweep_binary_search(
 @hydra.main(config_path="configs", version_base=None)
 def main(config):
     config = jax_extra.make_dataclass_from_dict(Config, config)
-    template_task_id = get_base_template_id(config)
+    project_name, task_name = get_task_details(config) 
+    print(f"{project_name=}")
+    print(f"{task_name=}")
+    template_task_id = Task.get_task(
+        project_name=project_name,
+        task_name=task_name,
+    ).id
+ 
     lower, upper = np.log10(5e-4), np.log10(5e-2)
-    lr_sweep_binary_search(lower, upper, template_task_id, queue_name=config.training.queue)
-    
+    lr_sweep_binary_search(config, lower, upper, template_task_id)
+        
 if __name__ == "__main__":
     main()
